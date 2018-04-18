46fdea0a3d3f8224b6e6dafb852bfeb031b9832f
Sangjun, Park <Sangjun, Park@TD3P127593>
2018-04-18 10:18:04 +0900

    Changing Jedis Connection Pool configuration

--- demo-redis-client/src/main/java/com/libedi/demo/RedisConfiguration.java
+++ demo-redis-client/src/main/java/com/libedi/demo/RedisConfiguration.java
@@ -2,7 +2,8 @@ package com.libedi.demo;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.jedis.JedisClientConfiguration;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
@@ -17,18 +18,30 @@ import org.springframework.data.redis.repository.configuration.EnableRedisReposi
 public class RedisConfiguration {
 	
 	@Bean
-	public RedisConnectionFactory redisConnectionFactory() {
+	public JedisConnectionFactory jedisConnectionFactory() {
 		JedisConnectionFactory factory = new JedisConnectionFactory();
-//		RedisStandaloneConfiguration config = factory.getStandaloneConfiguration();
-//		config.setHostName("localhost");	// default setting value
-//		config.setPort(6379);	// default setting value
+//		/*
+//		 * Change configuration Redis Server info
+//		 */
+//		RedisStandaloneConfiguration redisStandaloneConfiguration = factory.getStandaloneConfiguration();
+//		redisStandaloneConfiguration.setHostName("localhost");	// default setting value
+//		redisStandaloneConfiguration.setPort(6379);	// default setting value
+//		
+//		/**
+//		 * Change configuration Jedis Connection Pool info
+//		 */
+//		JedisClientConfiguration jedisClientConfiguration = factory.getClientConfiguration();
+//		jedisClientConfiguration.getPoolConfig().ifPresent(jedisPoolConfig -> {
+//			jedisPoolConfig.setMaxTotal(10);
+//			jedisPoolConfig.setMaxIdle(10);
+//		});
 		return factory;
 	}
 	
 	@Bean
 	public RedisTemplate<String, Object> redisTemplate() {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
-		template.setConnectionFactory(redisConnectionFactory());
+		template.setConnectionFactory(jedisConnectionFactory());
 		return template;
 	}
 }
